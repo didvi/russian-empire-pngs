fcf866c63f2a239016fa18138783be76b94e59ed
Divi Schmidt <divi@berkeley.edu>
2020-09-09 16:52:34 -0700

    implemented border detection for removing image borders

--- convert.py
+++ convert.py
@@ -3,10 +3,49 @@ import numpy as np
 import skimage as sk
 import skimage.io as skio
 from skimage import transform
+from skimage import feature
+from scipy import ndimage as ndi
+import matplotlib.pyplot as plt 
 
 import argparse
 import os
+import math
+import time
 
+def detect_borders_canny(img, sigma=3):
+    # did not work very well -- unused 
+    edges = feature.canny(img, sigma=sigma)
+    return edges
+
+def detect_borders_gradient(img, axis=1):
+    # calculates horizontal gradient across image in order to detect borders
+    def calc_grad(row):
+        before = row[:-2]
+        after = row[2:]
+        return after - before
+
+    grads = np.apply_along_axis(calc_grad, axis, img)
+    b_min = np.min(grads)
+    b_max = np.max(grads)
+    
+    grads = (grads - b_min) / (b_max - b_min) * 255
+    return grads > 150
+
+def remove_borders(img, edges):
+    """Naively removes borders on left and right side by removing all pixels between image edge and first detected edge
+    and all pixels between right image edge and last edge detected
+    """
+    MAX_BORDER_REMOVAL = 100
+    # column with largest border detections for the first 10th of the image
+    column_sum = np.sum(edges[:, :MAX_BORDER_REMOVAL], axis=0)
+    border_i = np.argmax(column_sum)
+
+    # column with largest border detections for the first 10th of the image
+    column_sum = np.sum(edges[:, -MAX_BORDER_REMOVAL:], axis=0)
+    right_border_i = np.argmax(column_sum) + (img.shape[1] - MAX_BORDER_REMOVAL)
+    
+    print("Removed borders at index: " + str((border_i, img.shape[1] - right_border_i)))
+    return img[:, border_i:right_border_i]
 
 def compute_metric(img_1, img_2, metric='ssd'):
     # Computes similarity metric for two images
@@ -59,7 +98,7 @@ def _align_pyramid(channel_1, channel_2, prev_displacement, scale, metric='ssd')
 def align(channel_1, channel_2, method='exhaustive', metric='ssd', max_offset=15):
     # Returns displacement (x, y) of channel 2 where channel 2 is offset to match channel 1 by some metric
     if method == 'pyramid':
-        initial_scale = 1/36
+        initial_scale = 1/(2**(math.log2(max_offset) // 1))
         initial_displacement = _best_displacement(sk.transform.rescale(
             channel_1, initial_scale), sk.transform.rescale(channel_2, initial_scale), (-2, -2), (2, 2), metric)
         
@@ -70,12 +109,19 @@ def align(channel_1, channel_2, method='exhaustive', metric='ssd', max_offset=15
 
     return (0, 0)
 
-def main(imname, method, metric, max_offset):
+def main(imname, method, metric, max_offset, border):
+    tic = time.time()
+
     # read in the image
     im = skio.imread(imname)
 
     # convert to double (might want to do this later on to save memory)
     im = sk.img_as_float(im)
+    
+    # remove borders that would throw off our image metrics
+    if not border:
+        edges = detect_borders_gradient(im)
+        im = remove_borders(im, edges)
 
     # compute the height of each part (just 1/3 of total)
     height = np.floor(im.shape[0] / 3.0).astype(np.int)
@@ -114,12 +160,15 @@ def main(imname, method, metric, max_offset):
 
     # save the image
     fname = "color/color_" + method + "_" + \
-        metric + "_" + os.path.basename(imname).split(".")[0] + '.jpg'
+        metric + "_" + str(border) + "_" + os.path.basename(imname).split(".")[0] + '.jpg'
     skio.imsave(fname, im_out)
 
+    toc = time.time()
+    print("Time elapsed: " + str(toc - tic))
+
     # display the image
-    skio.imshow(im_out)
-    skio.show()
+    # skio.imshow(im_out)
+    # skio.show()
 
 
 if __name__ == "__main__":
@@ -128,6 +177,18 @@ if __name__ == "__main__":
     ap.add_argument("--method", default='exhaustive')
     ap.add_argument("--metric", default='ssd')
     ap.add_argument("--offset", type=int, default=15)
+    ap.add_argument("--border", type=bool, default=False)
 
     args = ap.parse_args()
-    main(args.i, args.method, args.metric, args.offset)
+
+    if os.path.isdir(args.i):
+        for f in os.listdir(args.i):
+            if os.path.isfile(os.path.join(args.i, f)) :
+                try:
+                    print("Converting " + f)
+                    main(os.path.join(args.i, f), args.method, args.metric, args.offset, args.border)
+                except ValueError as e:
+                    print(e)
+                    continue
+    else:
+        main(args.i, args.method, args.metric, args.offset, args.border)
